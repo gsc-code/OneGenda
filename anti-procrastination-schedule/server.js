3236672e2b2d46fdc3a8713efd1316b80a4ca175
Ruohan Huang <ruohan.huang@outlook.com>
2023-08-25 10:31:47 -0700

    Serve static files using Express

--- anti-procrastination-schedule/server.js
+++ anti-procrastination-schedule/server.js
@@ -6,23 +6,8 @@ const app = express();
 // Use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// POST handler to read data
-// app.post('/login', (req, res) => {
-//     console.log({
-//       name: req.body.name,
-//       message: req.body.message
-//     });
-//     res.send('Thanks for your message!');
-// });
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
-});
-
-// Show form when user browses to /login
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/login.html'));
-});
+// Serving static files
+app.use('/', express.static(__dirname + '/views'));
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
