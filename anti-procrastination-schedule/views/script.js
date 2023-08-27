98d3693b8d06d0fa5d54ecd92d7f8a75b5833848
Ruohan Huang <ruohan.huang@outlook.com>
2023-08-27 11:19:11 -0700

    Layout and getting the profile icon to show :(

--- anti-procrastination-schedule/views/script.js
+++ anti-procrastination-schedule/views/script.js
@@ -1,7 +1,13 @@
 var responsePayload = {};
 const googleSignin = document.getElementById("google-signin");
 const dashboardButton = document.getElementById("dashboard");
-const profileIcon = document.getElementById("profile-icon");
+
+setWithExpiry("userID", responsePayload.sub, 3600000);
+setWithExpiry("userFullName", responsePayload.name, 3600000);
+setWithExpiry("userGivenName", responsePayload.given_name, 3600000);
+setWithExpiry("userFamilyName", responsePayload.family_name, 3600000);
+setWithExpiry("userImageURL", responsePayload.picture, 3600000);
+setWithExpiry("userEmail", responsePayload.email, 3600000);
 
 // Function to decode Google token
 function parseJwt (token) {
@@ -17,6 +23,7 @@ function parseJwt (token) {
 // Decode and separate user's credentials
 function handleCredentialResponse(response) {
     responsePayload = parseJwt(response.credential);
+    console.log(responsePayload);
     console.log("ID: " + responsePayload.sub);
     console.log('Full Name: ' + responsePayload.name);
     console.log('Given Name: ' + responsePayload.given_name);
@@ -26,9 +33,36 @@ function handleCredentialResponse(response) {
 
     googleSignin.hidden = true;
     dashboardButton.hidden = false;
-    profileIcon.setAttribute("src", responsePayload.picture);
-    profileIcon.hidden = false;
+    // profileIcon1.setAttribute("src", responsePayload.picture);
+    // profileIcon1.hidden = false;
 }
+
+
+function updateSignedIn() {
+    if (window.location.href == "https://unique-pixel-396900.uw.r.appspot.com/index.html" || window.location.href == "https://unique-pixel-396900.uw.r.appspot.com") {
+        const profileIcon1 = document.getElementById("profile-icon1");    
+        if (localStorage.getItem("userImageURL")) {
+            googleSignin.hidden = true;
+            dashboardButton.hidden = false;
+            profileIcon1.setAttribute("src", responsePayload.picture);
+            profileIcon1.hidden = false;
+        } else {
+            googleSignin.hidden = false;
+            dashboardButton.hidden = true;
+            profileIcon1.hidden = true;
+        }
+        
+    } else if (window.location.href == "https://unique-pixel-396900.uw.r.appspot.com/dashboard.html") {
+        const profileIcon2 = document.getElementById("profile-icon2");
+        if (localStorage.getItem("userImageURL")) {
+            profileIcon2.setAttribute("src", responsePayload.picture);
+            profileIcon2.hidden = false;
+        } else {
+            window.location.replace("https://unique-pixel-396900.uw.r.appspot.com");
+        }
+    }
+}
+
 
 // Set expiration time for itmes in the browser's local storage
 function setWithExpiry(key, value, ttl) {
@@ -60,16 +94,9 @@ function getWithExpiry(key) {
 	return item.value;
 }
 
-const userID = responsePayload.sub;
-const userFullName = responsePayload.name;
-const userGivenName = responsePayload.given_name;
-const userFamilyName = responsePayload.family_name;
-const userImageURL = responsePayload.picture;
-const userEmail = responsePayload.email;
 
-setWithExpiry("userID", userID, 3600000);
-setWithExpiry("userFullName", userFullName, 3600000);
-setWithExpiry("userGivenName", userGivenName, 3600000);
-setWithExpiry("userFamilyName", userFamilyName, 3600000);
-setWithExpiry("userImageURL", userImageURL, 3600000);
-setWithExpiry("userEmail", userEmail, 3600000);
+
+
+// data-login_uri="https://unique-pixel-396900.uw.r.appspot.com/dashboard.html"
+// data-scope="https://www.googleapis.com/auth/calendar"
+
