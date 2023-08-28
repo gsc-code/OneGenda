afe7252535ec61e140fe276706823c7abede9958
Ruohan Huang <ruohan.huang@outlook.com>
2023-08-28 09:59:25 -0700

    UPDATE AND MERGE JS SCRIPT FILE

--- anti-procrastination-schedule/views/script.js
+++ anti-procrastination-schedule/views/script.js
@@ -1,4 +1,13 @@
+// Lines 1-115: ruohan's workspace
+// Lines 120-175: gary's workspace
+
 var responsePayload = {};
+var userID;
+var userFullName;
+var userGivenName;
+var userFamilyName;
+var userImageURL;
+var userEmail;
 const googleSignin = document.getElementById("google-signin");
 const dashboardButton = document.getElementById("dashboard");
 
@@ -17,12 +26,26 @@ function parseJwt (token) {
 function handleCredentialResponse(response) {
     responsePayload = parseJwt(response.credential);
     console.log(responsePayload);
-    console.log("ID: " + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
-    console.log("Image URL: " + responsePayload.picture);
-    console.log("Email: " + responsePayload.email);
+    userID = responsePayload.sub;
+    userFullName = responsePayload.name;
+    userGivenName = responsePayload.given_name;
+    userFamilyName = responsePayload.family_name;
+    userImageURL = responsePayload.picture;
+    userEmail = responsePayload.email;
+    
+    setWithExpiry("userID", userID, 3600000); // 1 hr = 3600000
+    setWithExpiry("userFullName", userFullName, 3600000);
+    setWithExpiry("userGivenName", userGivenName, 3600000);
+    setWithExpiry("userFamilyName", userFamilyName, 3600000);
+    setWithExpiry("userImageURL", userImageURL, 10000); // test - 10 sec
+    setWithExpiry("userEmail", userEmail, 3600000);
+
+    console.log("ID: " + userID);
+    console.log('Full Name: ' + userFullName);
+    console.log('Given Name: ' + userGivenName);
+    console.log('Family Name: ' + userFamilyName);
+    console.log("Image URL: " + userImageURL);
+    console.log("Email: " + userEmail);
 
     googleSignin.hidden = true;
     dashboardButton.hidden = false;
@@ -31,11 +54,11 @@ function handleCredentialResponse(response) {
 
 function updateSignedIn() {
     if (window.location.href == "https://unique-pixel-396900.uw.r.appspot.com/index.html" || window.location.href == "https://unique-pixel-396900.uw.r.appspot.com") {
-        const profileIcon1 = document.getElementById("profile-icon1");    
-        if (localStorage.getItem("userImageURL")) {
+        var profileIcon1 = document.getElementById("profile-icon1");    
+        if (localStorage.userImageURL) {
             googleSignin.hidden = true;
             dashboardButton.hidden = false;
-            profileIcon1.setAttribute("src", responsePayload.picture);
+            profileIcon1.setAttribute("src", userImageURL);
             profileIcon1.hidden = false;
         } else {
             googleSignin.hidden = false;
@@ -44,9 +67,9 @@ function updateSignedIn() {
         }
         
     } else if (window.location.href == "https://unique-pixel-396900.uw.r.appspot.com/dashboard.html") {
-        const profileIcon2 = document.getElementById("profile-icon2");
-        if (localStorage.getItem("userImageURL")) {
-            profileIcon2.setAttribute("src", responsePayload.picture);
+        var profileIcon2 = document.getElementById("profile-icon2");
+        if (localStorage.userImageURL) {
+            profileIcon2.setAttribute("src", userImageURL);
             profileIcon2.hidden = false;
         } else {
             window.location.replace("https://unique-pixel-396900.uw.r.appspot.com");
@@ -78,7 +101,7 @@ function getWithExpiry(key) {
 		localStorage.removeItem(key);
         googleSignin.hidden = false;
         dashboardButton.hidden = true;
-        profileIcon.hidden = false;
+        profileIcon.hidden = true;
 		return null;
 	}
 
@@ -91,16 +114,61 @@ function getWithExpiry(key) {
 // data-login_uri="https://unique-pixel-396900.uw.r.appspot.com/dashboard.html"
 // data-scope="https://www.googleapis.com/auth/calendar"
 
-var userID = "";
-var userFullName = "";
-var userGivenName = "";
-var userFamilyName = "";
-var userImageURL = "";
-var userEmail = "";
 
-setWithExpiry("userID", responsePayload.sub, 3600000);
-setWithExpiry("userFullName", responsePayload.name, 3600000);
-setWithExpiry("userGivenName", responsePayload.given_name, 3600000);
-setWithExpiry("userFamilyName", responsePayload.family_name, 3600000);
-setWithExpiry("userImageURL", responsePayload.picture, 3600000);
-setWithExpiry("userEmail", responsePayload.email, 3600000);
+
+
+// Pre-algorithmic backend work (gary)
+// NOTE: pls replace the proper user-id info for variables and data cus idk how to do it
+
+const dayHours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+var availableHours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+var occupiedHours = [];
+var tasks = []; //format is [task-name, start-hour, end-hour, etc.]
+
+// Function for creating new task
+// checks if task hours overlaps with any previous tasks
+// returns [task-name, start-hour, end-hour] if hours don't overlap
+function newTask(taskName, startHour, endHour) {
+    var taskHours = [];
+    // creates array of occupying hours of new task
+    for (let i = startHour; i < endHour+1; i++) {
+        taskHours.push(i);
+    }
+    // checks for any overlapping hours
+    for (let i = 0; i < taskHours.length/3; i++) {
+        let item = taskHours[i];
+        for (let j = 0; j < occupiedHours.length; j++) {
+            if (item == occupiedHours[j]) {
+                // returns "unavailable" if hours overlap
+                return "unavailable";
+            }
+        }
+
+    }
+    // updates list of occupied hours
+    occupiedHours = occupiedHours.concat(taskHours);
+    sortHours(occupiedHours);
+    // updates list of available hours
+    for (let i = 0; i < taskHours.length; i++) {
+        let item = taskHours[i];
+        for (let j = 0; j < availableHours.length; j++) {
+            if (item == availableHours[j]) {
+                availableHours.splice(j, 1);
+            }
+
+        }
+
+    }
+    return [taskName, startHour, endHour];
+}
+
+function sortHours(array) {
+    array.sort((a, b) => a - b);
+    console.log(array);
+}
+
+function newSchedule() {
+
+}
+
+
