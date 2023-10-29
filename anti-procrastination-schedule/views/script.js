d62cba80adfa2c1135dae115678ce193a148d7ec
Ruohan Huang <ruohan.huang@outlook.com>
2023-10-29 16:16:58 -0700

    Fix

--- anti-procrastination-schedule/views/script.js
+++ anti-procrastination-schedule/views/script.js
@@ -547,11 +547,10 @@ async function addGEvent(taskName, startHour, endHour) {
         'calendarId': 'primary',
         'resource': event
       });
-      /*
+
       request.execute(function(event) {
-        appendPre('Event created: ' + event.htmlLink);
+        console.log('Event created: ' + event.htmlLink);
       });
-      */
 }
 
-// console.log(calanderList.get());
+// console.log(calendarList.get());
